cd4fb6d73e678e8a251eb946964021a4ca1eef5f
Markus Langenegger <langenegger_ma@bluewin.ch>
2023-05-12 18:03:21 +0200

    added Blog, Repository, Service and Test Class from lession

---
+++ src/main/java/ch/langenegger/Blog.java
@@ -0,0 +1,28 @@
+package ch.langenegger;
+
+public class Blog {
+
+    private String title;
+    private String content;
+
+    Blog(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/ch/langenegger/BlogRepository.java
@@ -0,0 +1,26 @@
+package ch.langenegger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class BlogRepository {
+
+    private List<Blog> blogs = new ArrayList<>();
+
+    public BlogRepository() {
+        blogs.add(new Blog("First Blog", "This is my first blog"));
+        blogs.add(new Blog("Second Blog", "This is my second blog"));
+    }
+
+    public List <Blog> getBlogs() {
+        return blogs;
+    }
+
+    public void addBlog(Blog blog) {
+        blogs.add(blog);
+    }
+
+}
